7df83e41fd98f4f347c216c6f29fb52656a6cf34
infoharry99 <infoharry99@gmail.com>
2026-04-04 22:56:45 +0530

    Sd

--- resources/views/admin/inventory/tabindex.blade.php
+++ resources/views/admin/inventory/tabindex.blade.php
@@ -621,7 +621,7 @@
         responsive: false,
         autoWidth: false,
         fixedHeader: true,
-         lengthMenu: [5,10,25,50,100,500,1000,5000,10000,50000,100000,500000,1000000],
+         lengthMenu: [2,5,10,25,50,100],
     
         /* Restore DB net_payable after DT render */
         initComplete: function () {
