06899b6a014da3dc13441ac58f2d6291248123a2
infoharry99 <infoharry99@gmail.com>
2026-04-05 15:36:37 +0530

    Sd

--- resources/views/admin/schedule_work_items/index.blade.php
+++ resources/views/admin/schedule_work_items/index.blade.php
@@ -146,7 +146,14 @@
     {{-- @endforeach --}}
 
 </div>
+<!-- jQuery (required for most DataTables setups) -->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 
+<!-- DataTables CSS -->
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
+
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
 <script>
 
     document.addEventListener('input', function (e) {
